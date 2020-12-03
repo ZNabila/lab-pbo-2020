95a23001b679194e257336874575ef7938b9d844
ZNabila <72507557+ZNabila@users.noreply.github.com>
2020-12-03 20:42:51 +0700

    Add files via upload
    Tugas 1

---
+++ Penumpang.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Penumpang here.
+ *
+ * @author (Zulfa Nabila)
+ * @version (1908107010011)
+ */
+public class Penumpang
+{
+    // instance variables - replace the example below with your own
+    private String nama;
+    private int umur;
+    private boolean hamil;
+
+    /**
+     * Constructor for objects of class Penumpang
+     */
+    public Penumpang(String nama,int umur, boolean hamil)
+    {
+        // initialise instance variables
+        this.nama = nama;
+        this.umur = umur;
+        this.hamil = hamil;
+    }
+
+    public String getNama()
+    { 
+        return this.nama;
+    }
+    public int getUmur()
+    {
+        return this.umur;
+    }
+    public boolean getHamil()
+    {
+        return this.hamil;
+    }
+}
